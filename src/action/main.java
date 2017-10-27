265397f090e675464f01dd9b5e99aee87a3d541f
BFH1122 <2504410511@qq.com>
2017-10-28 00:01:52 +0800

    无

---
+++ src/action/main.java
@@ -0,0 +1,13 @@
+package action;
+
+import java.sql.SQLException;
+
+public class main {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		editPeople.count_People();
+		//editPeople.add_People();
+	}
+
+}
