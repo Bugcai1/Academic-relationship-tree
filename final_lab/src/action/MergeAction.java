54f9055b7bb858fb47b2160e2c81d1e2dd6780e5
Huang <2236146406@qq.com>
2017-12-05 00:35:23 +0800

    Clear up some bugs

--- final_lab/src/action/MergeAction.java
+++ final_lab/src/action/MergeAction.java
@@ -23,13 +23,23 @@ public class MergeAction {
 	
 	///////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////用户被提示错误修改后的关系队列获取，修改的关系还是按照原先的1号和2号的顺序，方便更新关系表。。。。。。。
-	private ArrayList<Relation> amendQue;                  //前端的命名：amendQue
+	private ArrayList<Relation> amendQueFinal = new ArrayList<Relation>();                  
+	private String amendQue;
 	
-	public ArrayList<Relation> getAmendQue() {
+	public String getAmendQue() {
 		return amendQue;
 	}
-	public void setAmendQue(ArrayList<Relation> amendQue) {
-		this.amendQue = amendQue;
+	public void setAmendQue(String amendQue) {
+		this.amendQue=amendQue;
+	}
+	
+	public void editAmendQueFinal(JSONArray jsonArray) {
+		Relation rltTmp = null;
+		for(int i = 0 ;i<jsonArray.size();i++) {
+		    JSONObject jsonObject=JSONObject.fromObject(jsonArray.get(i));
+		    rltTmp = new Relation(jsonObject.getInt("no1"),jsonObject.getInt("no2"),jsonObject.getInt("type"),jsonObject.getString("start_time"),jsonObject.getString("end_time"));
+		    amendQueFinal.add(rltTmp);
+		}
 	}
 	////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////
@@ -123,12 +133,29 @@ public class MergeAction {
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////用户被提示后的修改冲突后的合并，以用户的修改表和自身表为准
 	public void amendMerge() throws SQLException, IOException{
+		//对于用户提交修改后的合并强制进行
 		
 		HttpServletResponse response=ServletActionContext.getResponse(); 
 		response.setContentType("text/html;charset=utf-8");
 	    PrintWriter out = response.getWriter();
 	    
-	    System.out.println("amend"+getAmendQue());
+	    //Just print the amendQue .........................................................................
+	    System.out.println("========================================");
+	    System.out.println(amendQue);
+	    
+	    JSONObject jsonObject=JSONObject.fromObject(amendQue);
+	    JSONArray jsonArray = (JSONArray) jsonObject.get("rows");
+	    editAmendQueFinal(jsonArray);
+	    
+	    for(Relation rltTmp : amendQueFinal) {
+	    	System.out.println(rltTmp.no1);
+	    	System.out.println(rltTmp.no2);
+	    	System.out.println(rltTmp.relationType);
+	    	System.out.println(rltTmp.startTime);
+	    	System.out.println(rltTmp.endTime);
+	    	System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+	    }
+	    //................................................................
 	    
 		DbUtil con1=new DbUtil();
 		DbUtil con2=new DbUtil();
@@ -165,14 +192,16 @@ public class MergeAction {
 		if(Other.MixRelation(ID1Que, ID2Que)) {
 			Other.cmpRltQue(ID1Que, ID2Que);
 			
+			//删除冲突队列读应的关系
+			Other.deleteMllQue(ID1Que.rltMllQue, ID1Table);
+			
 			//将rltAddQue的数据加入数据库中
 			Other.addRltAddQue(ID1Que.rltAddQue, ID1Table);
 			
-			//将用户提交的修改后的关系表对ID1表进行强制更新	
-			for(Relation rltTmp:this.amendQue) {
-				String updateSQL = "update "+ID1Table+" set relation="+rltTmp.relationType+",start_time='"+rltTmp.startTime+"',end_time='"+rltTmp.endTime
-						+"' where user_id="+rltTmp.no1+" and relation_id="+rltTmp.no2+";";
-				con1.executeUpdate(updateSQL);
+			//将用户提交的修改后的关系表对ID1表进行加入	
+			for(Relation rltTmp:this.amendQueFinal) {
+				String addSQL = "insert into "+ID1Table+" values("+rltTmp.no1+","+rltTmp.no2+","+rltTmp.relationType+",'"+rltTmp.startTime+"','"+rltTmp.endTime+"');";
+				con1.executeUpdate(addSQL);
 			}
 				
 			out.println(0);
@@ -194,10 +223,10 @@ class Relation{
 	public int no1;
 	public int no2;
 	public int relationType;
-	public int startTime;
-	public int endTime;
+	public String startTime;
+	public String endTime;
 	
-	public Relation(int no1, int no2, int relationType, int startTime, int endTime) {
+	public Relation(int no1, int no2, int relationType, String startTime, String endTime) {
 		this.no1=no1;
 		this.no2=no2;
 		this.relationType=relationType;
@@ -220,13 +249,13 @@ class Relation{
 		if(no1 == obj.no1) {
 			if(no2 == obj.no2) {
 				if(relationType == obj.relationType) {
-					if(startTime == obj.startTime && endTime==obj.endTime) {
+					if(startTime.equals(obj.startTime) && endTime.equals(obj.endTime)) {
 						clash =  0;
 					}else {
 						clash = 4;
 					}
 				}else {
-						if(startTime == obj.startTime && endTime==obj.endTime) {
+					if(startTime.equals(obj.startTime) && endTime.equals(obj.endTime)) {
 						clash = 3;
 					}else {
 						clash = 2;
@@ -242,13 +271,13 @@ class Relation{
 		if(no1 == obj.no2) {
 			if(no2 == obj.no1) {
 				if(relationType + obj.relationType == 4) {
-					if(startTime == obj.startTime && endTime==obj.endTime) {
+					if(startTime.equals(obj.startTime) && endTime.equals(obj.endTime)) {
 						clash =  1;
 					}else {
 						clash = 4;
 					}
 				}else {
-						if(startTime == obj.startTime && endTime==obj.endTime) {
+					if(startTime.equals(obj.startTime) && endTime.equals(obj.endTime)) {
 						clash = 3;
 					}else {
 						clash = 2;
@@ -290,7 +319,7 @@ class RelationQue{
 	
 	public RelationQue(ResultSet rst) throws SQLException {
 		while(rst.next()) {
-			Relation tmp = new Relation(rst.getInt(1), rst.getInt(2), rst.getInt(3), rst.getInt(4), rst.getInt(5));
+			Relation tmp = new Relation(rst.getInt(1), rst.getInt(2), rst.getInt(3), rst.getString(4), rst.getString(5));
 			rltQue.add(tmp);
 		}
 	}
@@ -339,11 +368,12 @@ class Other {
 	}
 	
 	//将关系队列中的待加入队列加入数据库中
-	public static void addRltAddQue(Queue<Relation> rltAddQue, String Table) {
+	public static void addRltAddQue(Queue<Relation> rltAddQue, String tableName) {
 		String AddSQL= null;
 		DbUtil con=new DbUtil();
 		for(Relation rltTmp : rltAddQue) {
-			AddSQL="insert into "+Table+" values("+rltTmp.no1+","+rltTmp.no2+","+rltTmp.relationType+","+rltTmp.startTime+","+rltTmp.endTime+");";
+			AddSQL="insert into "+tableName+" values("+rltTmp.no1+","+rltTmp.no2+","+rltTmp.relationType+",'"+rltTmp.startTime+"','"+rltTmp.endTime+"');";
+			System.out.println(AddSQL+"+++++++++++++++++++++++++++++++++++++++++++++");
 			con.executeUpdate(AddSQL);
 		}
 	}
@@ -352,73 +382,57 @@ class Other {
 	public static JSONArray sendError(Queue<ClashRelation> rltMllQue,int ID1,int ID2) throws IOException, SQLException {
 		JSONArray jsonArray=new JSONArray();
 		JSONObject jsonObject=new JSONObject();
-
-
+		
 		DbUtil con=new DbUtil();
 		ResultSet rst=null;
 
-		
 		for(ClashRelation rltTmp : rltMllQue) {
 			jsonObject = new JSONObject();
 			/*查两人的相关信息*/
 			//姓名：
+			
 			String sql="select * from register_person where id="+rltTmp.no1+";";
-			int flag=0;
 			rst=con.executeQuery(sql);
-			while(rst.next())
-			{
-				flag=1;
+			if(rst.next()){
 				jsonObject.put("user_1",rst.getString("name"));
-			}
-			if(flag==0)
-			{
+			}else{
 				sql="select * from no_register_person where id="+rltTmp.no1+";";
 				rst=con.executeQuery(sql);
-				while(rst.next())
+				if(rst.next()) {
 					jsonObject.put("user_1",rst.getString("name"));
+				}
 			}
+			
 			sql="select * from register_person where id="+rltTmp.no2+";";
 			rst=con.executeQuery(sql);
-			flag=0;
-			while(rst.next())
-			{
-				flag=1;
+			if(rst.next()){
 				jsonObject.put("user_2",rst.getString("name"));
-			}
-			if(flag==0)
-			{
+			}else {
 				sql="select * from no_register_person where id="+rltTmp.no2+";";
 				rst=con.executeQuery(sql);
-				while(rst.next())
+				if(rst.next()) {
 					jsonObject.put("user_2",rst.getString("name"));
+				}
 			}
-			
-			
 			
 			jsonObject.put("no1",rltTmp.no1);
 			jsonObject.put("no2", rltTmp.no2);
 
-
 			sql="select * from register_person where id="+ID1+";";
 			rst=con.executeQuery(sql);
-			System.out.println(sql);
 			String phone="";
-			while(rst.next())
-			{
+			if(rst.next()) {
 			  phone=rst.getString(5);
 			}
-			String Tablename="a"+ID1+phone;
-
-			sql="select * from "+Tablename+" where user_id="+rltTmp.no1+" and relation_id="+rltTmp.no2+";";
+			String ID1Tablename="a"+ID1+phone;
+			sql="select * from "+ID1Tablename+" where user_id="+rltTmp.no1+" and relation_id="+rltTmp.no2+";";
 			rst=con.executeQuery(sql);
-			System.out.println(sql);
-			while(rst.next())
-			{
+			while(rst.next()) {
 				jsonObject.put("type",rst.getString(3));
 				jsonObject.put("start_time",rst.getString(4));
 				jsonObject.put("end_time",rst.getString(5));
 			}
-			
+
 			// jsonObject.put("rltTypeClash",rltTmp.RltTypeClash);
 			// jsonObject.put("rltTimeClash",rltTmp.RltTimeClash);//1表示冲突
 			jsonArray.add(jsonObject);
@@ -429,4 +443,15 @@ class Other {
 		
 		return jsonArray;
 	}
+	
+	//用户提交修改后的关系表后，进行原数据表中冲突的关系进行删除
+	public static void deleteMllQue(Queue<ClashRelation> MllQue,String tableName) throws SQLException{
+		DbUtil con=new DbUtil();
+		String deleteSQL = null;
+		for(ClashRelation cRltTmp : MllQue) {
+			deleteSQL="delete from "+tableName+" where user_id="+cRltTmp.no1+" and relation_id="+cRltTmp.no2+";";
+			con.executeUpdate(deleteSQL);
+		}
+	}
+	
 }
